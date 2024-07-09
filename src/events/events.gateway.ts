b0536ee88e9ffac1c8d55597f6119f47b0d67396
박현민 <dndb4902@naver.com>
2024-07-09 19:04:11 +0900

    # room & game event upd

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -32,45 +32,56 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
     this.logger.log(`socket connected: ${socket.id}`);
   }
 
-  // disconnect 되면 강퇴해야지 않을까?
   handleDisconnect(@ConnectedSocket() socket: Socket) {
     this.logger.log(`socket disconnected: ${socket.id}`);
   }
 
-  // connection 후 콜백 함수
+
   @SubscribeMessage('room_create')
-  handleConnectCheck(@MessageBody() message:string) {
-    this.logger.log(message);
+  handleRoomCreate(@MessageBody() message:string) {
+    this.logger.log(`room_create: ${JSON.stringify(message)}`);
+  }
+
+  @SubscribeMessage('room_join')
+  handleRoomJoin(@MessageBody() message:string, @ConnectedSocket() client: Socket)  {
+    this.logger.log(`room_join: ${JSON.stringify(message)}`);
+    client.broadcast.emit('room_join', message);
+  }
+
+  @SubscribeMessage('room_exit')
+  handleRoomExit(@MessageBody() message:string) {
+    this.logger.log(`room_exit: ${JSON.stringify(message)}`);
   }
 
 
   // game 생성 -> Room에 따른 drawer, watcher 결정
-  @SubscribeMessage('game_join')
-  handleGameJoin(@MessageBody() message:string)  {
-    this.logger.log(message);
+  @SubscribeMessage('game_start')
+  handleGameJoin(@MessageBody() message:number, @ConnectedSocket() client: Socket)  {
+    this.logger.log(`game_start: ${JSON.stringify(message)}`);
+    client.broadcast.emit('game_start', message);
   }
 
   // game turn 종료 시 -> drawer, watcher 변경 -> api에서 와야 하나?
-  @SubscribeMessage('game_turn_change')
-  handleGameTurnChange(@MessageBody() message:string) {
-    this.logger.log(message);
+  @SubscribeMessage('game_round_change')
+  handleGameTurnChange(@MessageBody() message:string, @ConnectedSocket() client: Socket)  {
+    this.logger.log(`game_round_change: ${JSON.stringify(message)}`);
+    client.broadcast.emit('game_round_change', message);
   }
 
-  //fixme 필요한가??
   @SubscribeMessage('game_end')
   handleGameEnd(@MessageBody() message:string) {
-    this.logger.log(message);
+    this.logger.log(`game_end: ${JSON.stringify(message)}`);
   }
 
   // drawer : 그림 그리기 / watcher : 그림 그리는 좌표 전송
   @SubscribeMessage('drawer_draw_start')
-  handleDrawerDrawStart(@MessageBody() data: PointData[],  @ConnectedSocket() client: Socket): void {
+  handleDrawerDrawStart(@MessageBody() data: PointData[],  @ConnectedSocket() client: Socket) {
     // 여기서 받은 좌표를 처리합니다.
     client.broadcast.emit('drawer_draw_start', data);
   }
 
   @SubscribeMessage('drawer_draw_move')
-  handleDrawerDrawMove(@MessageBody() data: number[],  @ConnectedSocket() client: Socket): void {
+  handleDrawerDrawMove(@MessageBody() data: number[],  @ConnectedSocket() client: Socket) {
     // 여기서 받은 좌표를 처리합니다.
     client.broadcast.emit('drawer_draw_move', data);
   }
