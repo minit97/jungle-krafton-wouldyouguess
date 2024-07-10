76888292abfa08e599ffa8889aa77468898100ab
Cha <tjddnr9553@naver.com>
2024-07-10 16:10:34 +0900

    feat : 친구 초대기능
    feat : 친구 초대기능

--- src/main/java/com/krafton/api_server/api/room/dto/RoomRequest.java
+++ src/main/java/com/krafton/api_server/api/room/dto/RoomRequest.java
@@ -1,14 +1,12 @@
 package com.krafton.api_server.api.room.dto;
 
-import com.krafton.api_server.api.game.domain.GameType;
 import lombok.Getter;
-
-import java.util.List;
 
 public class RoomRequest {
 
     @Getter
     public static class RoomCreateRequest {
         private Long userId;
+        private Long roomId;
     }
 }
--- src/main/java/com/krafton/api_server/api/room/service/RoomService.java
+++ src/main/java/com/krafton/api_server/api/room/service/RoomService.java
@@ -23,37 +23,36 @@ public class RoomService {
     private final RoomRepository roomRepository;
 
     public Long createRoom(RoomCreateRequest roomCreateRequest) {
-        User user = userRepository.findById(roomCreateRequest.getUserId())
-                .orElseThrow(NoSuchElementException::new);
+        User user = userRepository.findById(roomCreateRequest.getUserId()).orElseThrow(NoSuchElementException::new);
 
         Room room = Room.builder()
                 .user(user)
                 .build();
-
         Room createdRoom = roomRepository.save(room);
         return createdRoom.getId();
     }
 
     public void joinRoom(Long roomId, RoomCreateRequest roomCreateRequest) {
-        Room room = roomRepository.findById(roomId)
-                .orElseThrow(NoSuchElementException::new);
-        User joinedUser = userRepository.findById(roomCreateRequest.getUserId())
-                .orElseThrow(NoSuchElementException::new);
+        Room room = roomRepository.findById(roomId).orElseThrow(NoSuchElementException::new);
+        User joinedUser = userRepository.findById(roomCreateRequest.getUserId()).orElseThrow(NoSuchElementException::new);
         room.joinRoom(joinedUser);
     }
 
 
     public void exitRoom(Long roomId, RoomCreateRequest roomCreateRequest) {
-        Room room = roomRepository.findById(roomId)
-                .orElseThrow(NoSuchElementException::new);
-        User exitedUser = userRepository.findById(roomCreateRequest.getUserId())
-                .orElseThrow(NoSuchElementException::new);
+        Room room = roomRepository.findById(roomId).orElseThrow(NoSuchElementException::new);
+        User exitedUser = userRepository.findById(roomCreateRequest.getUserId()).orElseThrow(NoSuchElementException::new);
 
         room.exitRoom(exitedUser);
         if (room.getParticipants().size() == 0) {
             roomRepository.delete(room);
         }
-
     }
 
+//    public List getParticipants(Long roomId) {
+//        System.out.println("roomId = " + roomId);
+//        Room room = roomRepository.findById(roomId).orElseThrow(NoSuchElementException::new);
+//        List<User> participants = room.getParticipants();
+//        return participants;
+//    }
 }
